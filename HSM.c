66b500ab36ae28da059740e8340bf49605d3d58b
Andrew Mikhail <andrewmikhail@ymail.com>
2017-04-08 16:59:54 -0700

    Update HSM.c

--- HSM.c
+++ HSM.c
@@ -21,16 +21,70 @@ Event RunHSM(Event ThisEvent){
 
 	switch(CurrentState){
 		case InitState:
+			if(ThisEvent->EventType == Init_Event){
+				//do initializations
+			}
+			break;
 
 		case SearchingForSignal:
+			if(ThisEvent->EventType == Ultrasonic_Event){
+				
+			}
+			else if(ThisEvent->EventType == Depth_Event){
+				
+			}
+			else if(ThisEvent->EventType == Humidity_Event){
+				nextState = Panic;
+			}
+			break;
 
 		case TrackingSignal:
+			if(ThisEvent->EventType == Ultrasonic_Event){
+				
+			}
+			else if(ThisEvent->EventType == Depth_Event){
+				
+			}
+			else if(ThisEvent->EventType == Humidity_Event){
+				nextState = Panic;
+			}
+			break;
 
 		case GoingUp:
+			if(ThisEvent->EventType == Ultrasonic_Event){
+				
+			}
+			else if(ThisEvent->EventType == Depth_Event){
+				
+			}
+			else if(ThisEvent->EventType == Humidity_Event){
+				nextState = Panic;
+			}
+			break;
 
 		case GoingDown:
+			if(ThisEvent->EventType == Ultrasonic_Event){
+				
+			}
+			else if(ThisEvent->EventType == Depth_Event){
+				
+			}
+			else if(ThisEvent->EventType == Humidity_Event){
+				nextState = Panic;
+			}
+			break;
 
 		case Panic:
+			if(ThisEvent->EventType == Ultrasonic_Event){
+				
+			}
+			else if(ThisEvent->EventType == Depth_Event){
+				
+			}
+			else if(ThisEvent->EventType == Humidity_Event){
+				nextState = Panic;
+			}
+			break;
 	}
 	return ThisEvent;
 }
