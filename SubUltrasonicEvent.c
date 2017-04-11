0d3628baab48738c7a362e9c71ba90aed57a16ec
Andrew Mikhail <andrewmikhail@ymail.com>
2017-04-10 18:56:03 -0700

    Add Motor Directives Based on Ultrasonic Sensor Triggering

--- SubUltrasonicEvent.c
+++ SubUltrasonicEvent.c
@@ -19,13 +19,8 @@ typedef enum {
     RightOn,
 } SubUltrasonicEventHSMState;
 
-/* #define LeftPin
-   #define RightPin
-   #define TopPin
-   #define BottomPin
-   */
-
 static SubUltrasonicEventHSMState CurrentState;
+int right, left, up, down, tracking, moving;
 
 #define LEFT_ON		3
 #define RIGHT_ON	2
@@ -52,50 +47,84 @@ Event RunSubUltrasonicEventHSM(Event ThisEvent) {
 
             break;
         case Idle: // If current state is initial State
-            /* if TopPin goes high 
-                    nextState = TopOn;
-                    break;
-               else if BottomPin goes high 
-                    nextState = BottomOn;
-                    break;
-               else if RightPin goes high
-                    nextState = RightOn;
-                    break;
-               else if LeftPin goes high
-                    nextState = LeftOn;
-                    break;
-                    */
-            break;
-
-        case TopOn: // If Top sensor goes high
-            /* Once TopPin goes low
-                nextState = Idle;
+            right  = digitalRead(rightPin);
+            left = digitalRead(leftPin);
+            up = digitalRead(upPin);
+            down = digitalRead(downPin);
+           
+            if(up){ // if Top Pin goes high
+                nextState = TopOn;
                 break;
-                */
+            }else if(down) //if Bottom Pin goes high 
+                nextState = BottomOn;
+                break;
+            }else if(right) // if Right Pin goes high
+                nextState = RightOn;
+                break;
+            }else if(left) // if LeftPin goes high
+                nextState = LeftOn;
+                break;
+            }  
+            break;
+        
+        case TopOn: // If Top sensor goes high
+            moving = 1;
+            while(moving)
+                up = digitalRead(upPin);
+                if(up) dive(-10); //rise by moving motors 10 percent
+                if(!up){
+                    dive(0); //stop motors from rising
+                    moving = 0;
+                }
+            }
+            // Once TopPin goes low
+            nextState = Idle;
+              
             break;
 
         case BottomOn: // If bottom sensor goes high
-            /* Once BottomPin goes low
-                nextState = Idle;
-                break;
-                */
+            moving = 1;
+            while(moving)
+                down = digitalRead(downPin);
+                if(down) dive(10); //dive by moving motors 10 percent
+                if(!down){
+                    dive(0); //stop motors from diving
+                    moving = 0;
+                }
+            }
+            // Once BottomPin goes low
+            nextState = Idle;
             break;
 
         case LeftOn: // If left sensor goes high
-            /* Once LeftPin goes low
-                nextState = Idle;
-                break;
-                */
+            moving = 1;
+            while(moving)
+                left = digitalRead(leftPin);
+                if(left) turnLeft(10); //turn left by moving motors 10 percent
+                if(!left){
+                    turnLeft(0); //stop motors from turning
+                    moving = 0;
+                }
+            }
+            // Once LeftPin goes low
+            nextState = Idle;
             break;
 
         case RightOn: // If right sensor goes high
-            /* Once RightPin goes low
-                nextState = Idle;
-                break;
-                */
+            moving = 1;
+            while(moving)
+                right = digitalRead(rightPin);
+                if(right) turnRight(10); //turn right by moving motors 10 percent
+                if(!right){
+                    turnRight(0); //stop motors from turning
+                    moving = 0;
+                }
+            }
+            // Once RightPin goes low
+            nextState = Idle;
             break;
 
-        default:
+        default://NOT SURE WHAT THIS IS?
             break;
 
     }
