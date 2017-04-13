49d3f906053d127813772e3867a847de5b88bae7
Andrew Mikhail <andrewmikhail@ymail.com>
2017-04-13 15:47:29 -0700

    Add Ultrasonic Event Checker

---
+++ UltrasonicEventChecker.h
@@ -0,0 +1,14 @@
+/* UltrasonicEventChecker.h
+ * Underwater Drone/ROV
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "EventFramework.h"
+#include "HSM.h"
+#include "eventQueue.h"
+#include "SubUltrasonicEvent.h"
+#include "autoMotorFunctions.h"
+#include "UltrasonicEventChecker.h"
+
+Event checkUltrasonicSensors();
