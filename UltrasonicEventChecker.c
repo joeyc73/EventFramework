d1c6c025881c88764e766cbc5729115315ed1172
Andrew Mikhail <andrewmikhail@ymail.com>
2017-04-13 16:06:32 -0700

    Edits to Ultrasonic Event Checker

--- UltrasonicEventChecker.c
+++ UltrasonicEventChecker.c
@@ -13,6 +13,7 @@
 #include "UltrasonicEventChecker.h"
 
 uint8_t lastState = 0;
+Event thisEvent;
 
 Event checkUltrasonicSensors(){
 	uint8_t currentState = lastState;
@@ -20,8 +21,17 @@ Event checkUltrasonicSensors(){
 	right = digitalRead(rightPin);
 	top = digitalRead(topPin);
 	bottom = digitalRead(bottomPin);
-	currentState = left << right << top << bottom;
-	if(currentState != lastState) return Ultrasonic_Event;
+	tracking = digitalRead(beaconPin);
+	currentState = (left<<3) | (right<<2) | (top<<1) | bottom; //combine 4 digit reading
+	if(currentState != lastState){ 
+		thisEvent.Eventype = Ultrasonic_Event;
+		thisEvent.Param = currentState;
+		return thisEvent;
+	}else if(!tracking){
+		thisEvent.EventType = Ultrasonic_Event;
+		thisEvent.Param = 0; //lost signal
+		return thisEvent;
+	}
 	lastState = currentState;
 	return No_Event;
 }
